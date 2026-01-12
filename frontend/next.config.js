25b6800963ab5a875bb34334725f54c594c4de87
ngonko <christiangonko@yahoo.fr>
2026-01-13 00:05:02 +0100

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost',
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 }
 
